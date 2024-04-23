08b511ea7a2c05cc062d325028e32cd0145d5bd3
InstantMain <veryzcommon@gmail.com>
2024-04-22 22:42:28 -0700

    home page terminal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,87 @@
 <script lang="ts">
     import "../lib/styles/home.scss"
-    import Counter from "$lib/components/Counter.svelte"
 </script>
 
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:wght@400;700&display=swap" rel="stylesheet">
+</svelte:head>
+
 <main>
-    <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-            <img src="/vite.svg" class="logo" alt="Vite Logo" />
-        </a>
-        <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-            <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-        </a>
-    </div>
-    <h1>Vite + Svelte</h1>
-
-    <div class="card">
-        <Counter />
-    </div>
-
-    <p>
-        Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-    </p>
-
-    <p class="read-the-docs">
-        Click on the Vite and Svelte logos to learn more
-    </p>
+    <section id="home">
+        <div id="terminal">
+            <div id="header">
+                <p>root@an-cyber.pages.dev: ~</p>
+            </div>
+            <div id="body">
+                <p>root@an-cyber.pages.dev:<span style="color:#464c6f">~</span><span style="font-weight:400;color:#FFFFFF">$</span></p>
+            </div>
+        </div>
+    </section>
 </main>
+
+<style lang="scss">
+    #home {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: linear-gradient(to top right, #83123d, #cb4015);
+        background-size: cover;
+    }
+
+    #terminal {
+        display: flex;
+        position: relative;
+        place-content: center;
+        flex-wrap: wrap;
+        top: 8%;
+        left: 5%;
+        width: 90%;
+        height: 80%;
+
+        #header {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+            width: 100%;
+            height: 10%;
+            z-index: 1;
+            border-radius: 10px 10px 0px 0px;
+            background-color: #4b4841;
+
+            p {
+                text-overflow: ellipsis;
+                white-space: nowrap;
+                overflow: hidden;
+                font-size: 1rem;
+                color: #FFFFFF;
+            }
+        }
+        #body {
+            display: inline-block;
+            position: relative;
+            width: 100%;
+            height: 90%;
+            background-color: #2f0922;
+            overflow: hidden;
+
+            p {
+                text-align: left;
+                top: -1.1rem;
+                left: 0.1rem;
+                font-size: 1.1rem;
+                color: #97bc4a;
+            }
+        }
+
+        p {
+            position: relative;
+            font-family: Ubuntu Mono;
+            font-weight: 700;
+        }
+    }
+</style>
